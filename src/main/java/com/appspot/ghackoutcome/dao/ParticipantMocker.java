47cf94f9ebc5d339eaede0f9fe4e9f9898839d81
rac2030 <m@rac.su>
2014-06-21 14:52:17 +0200

    added DAO

---
+++ src/main/java/com/appspot/ghackoutcome/dao/ParticipantMocker.java
@@ -0,0 +1,12 @@
+package com.appspot.ghackoutcome.dao;
+
+import java.util.List;
+
+/**
+ * Created by rac on 21.06.14.
+ */
+public class ParticipantMocker {
+    public static List<Participant> getMockList() {
+        return null;
+    }
+}
